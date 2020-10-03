a4138f019fb6dbefe5773d55d19a654db1d600ed
leiyunkang <leiyunkang7@outlook.com>
2020-10-03 15:06:05 +0800

    7.2.2 add 方法

--- src/views/js-data-structures-algorithms/7/Set.ts
+++ src/views/js-data-structures-algorithms/7/Set.ts
@@ -1,10 +1,21 @@
-export default class Set<T = any> {
-  items: Record<string, T>
+type SetType = string | number
+
+export default class Set {
+  items: Record<SetType, SetType>
   constructor() {
     this.items = {}
   }
 
-  has(element: string) {
+  has(element: SetType) {
     return Reflect.has(this.items, element)
+  }
+
+  add(element: SetType) {
+    if (!this.has(element)) {
+      this.items[element] = element
+      return true
+    }
+
+    return false
   }
 }
