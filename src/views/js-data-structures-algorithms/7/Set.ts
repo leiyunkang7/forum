d9eada35c786a0dd5ee5634cbe50ef6eb19764c5
leiyunkang <leiyunkang7@outlook.com>
2020-10-03 14:46:27 +0800

    7.2.1 has(element) 方法

--- src/views/js-data-structures-algorithms/7/Set.ts
+++ src/views/js-data-structures-algorithms/7/Set.ts
@@ -1,6 +1,10 @@
-export default class Set {
-  items: {}
+export default class Set<T = any> {
+  items: Record<string, T>
   constructor() {
     this.items = {}
+  }
+
+  has(element: string) {
+    return Reflect.has(this.items, element)
   }
 }
